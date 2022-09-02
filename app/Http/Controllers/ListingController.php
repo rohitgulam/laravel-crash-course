afb73ee324eb2f1c6de06ddb9b4fa25e9ccf7c07
rohitgulam <rohitsgulam@gmail.com>
2022-09-02 15:02:10 +0300

    addded filter to Listingcontroller

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -9,9 +9,8 @@ class ListingController extends Controller
 {
     // Show all listings
     public function index(){
-        dd(request('tag'));
         return view('listings.index', [
-            'listings' => Listing::all()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
         ]);
     }
 
