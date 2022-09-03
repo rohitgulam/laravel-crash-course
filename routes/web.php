524cdb5dce3e0683c8e9a0e04e61bfbcd9b9fa17
rohitgulam <rohitsgulam@gmail.com>
2022-09-03 18:11:09 +0300

    created register, login and log out functionalitiea

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -55,3 +56,13 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 // Eloquent model used on ('/listings/{listing}', function(Listing $listing)
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+// Show register create form
+Route::get('/register', [UserController::class, 'create']);
+
+// create new user
+Route::post('/users', [UserController::class, 'store']);
+
+// Log out 
+Route::post('/logout', [UserController::class, 'logout']);
