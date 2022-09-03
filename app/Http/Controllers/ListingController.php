63a8627c1107207d01d7b1c637f3e17c24d278ea
rohitgulam <rohitsgulam@gmail.com>
2022-09-03 09:58:55 +0300

    added form functionality and pagination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
     // Show all listings
     public function index(){
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
     }
 
@@ -24,5 +25,26 @@ class ListingController extends Controller
     // Show create form
     public function create(){
         return view('listings.create');
+    }
+
+     // Store Listing data from form
+     public function store(Request $request){
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        Listing::create($formFields);
+
+        // Add flash message, first way 
+        // Session::flash('message', 'Listing created!');
+
+        // Or add a redirect with a flash message 
+        return redirect('/')->with('message', 'Listing created succesfully!');
     }
 }
