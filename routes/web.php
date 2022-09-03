76a57a8b505112fe64b4d08d0ad28538b4055550
rohitgulam <rohitsgulam@gmail.com>
2022-09-03 10:55:05 +0300

    added edit route

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,12 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // Store listing data
 Route::post('/listings', [ListingController::class, 'store']);
 
+// Show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 
 
 
